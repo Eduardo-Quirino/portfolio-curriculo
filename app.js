a8423ad56c7e246f01efff72ec9f35f21d18b208
Eduardo Quirino <edu.3532quirino@gmail.com>
2023-05-02 14:02:08 -0300

    Update

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // barra de navegação aderente no script de rolagem
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         //botão de rolagem para cima mostrar / ocultar script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
 
     // script slide-up
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         //removendo a rolagem suave no clique do botão deslizante
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // aplicando novamente a rolagem suave nos itens do menu, clique
         $('html').css("scrollBehavior", "smooth");
     });
 
     // menu de alternância / script da barra de navegação
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
     // digitando script de animação de texto
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer Web", "Freelancer","Front End", "Back End"],
+        strings: ["Developer", "Designer Web", "Freelancer", "Front End", "Back End"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer Web", "Freelancer","Front End", "Back End"],
+        strings: ["Developer", "Designer Web", "Freelancer", "Front End", "Back End"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -56,15 +56,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
