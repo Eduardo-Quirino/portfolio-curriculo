01c46ceb27fba1d3686f26759df8540c1846caf0
Eduardo Quirino <edu.3532quirino@gmail.com>
2023-08-07 14:15:16 -0300

    incluso curriculo novo 2023

--- email.php
+++ email.php
@@ -1,41 +1,50 @@
 <?php
-//Validação dos dados recebidos pelo formulário
-if (empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['assunto']) || empty($_POST['mensagem'])) {
-  echo "Por favor, preencha todos os campos do formulário";
-  exit;
-}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Validação dos dados recebidos pelo formulário
+  $requiredFields = ['nome', 'email', 'assunto', 'mensagem'];
 
-$nome = htmlspecialchars($_POST['nome'], ENT_QUOTES, 'UTF-8');
-$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$assunto = htmlspecialchars($_POST['assunto'], ENT_QUOTES, 'UTF-8');
-$mensagem = htmlspecialchars($_POST['mensagem'], ENT_QUOTES, 'UTF-8');
+  foreach ($requiredFields as $field) {
+    if (empty($_POST[$field])) {
+      echo "Por favor, preencha todos os campos do formulário";
+      exit;
+    }
+  }
 
-$data_envio = date('d/m/Y');
-$hora_envio = date('H:i:s');
+  $nome = htmlspecialchars($_POST['nome'], ENT_QUOTES, 'UTF-8');
+  $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+  $assunto = htmlspecialchars($_POST['assunto'], ENT_QUOTES, 'UTF-8');
+  $mensagem = htmlspecialchars($_POST['mensagem'], ENT_QUOTES, 'UTF-8');
 
-//Compo E-mail
-$arquivo = "
-  <html>
-    <p><b>Nome: </b>$nome</p>
-    <p><b>E-mail: </b>$email</p>
-    <p><b>Assunto: </b>$assunto</p>
-    <p><b>Mensagem: </b>$mensagem</p>
-    <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-  </html>
-";
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
 
-//Emails para quem será enviado o formulário
-$destino = "edu.3532quirino@gmail.com";
-$assunto = "Contato pelo Site Portfólio";
+  // Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Assunto: </b>$assunto</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
 
-//Este sempre deverá existir para garantir a exibição correta dos caracteres
-$headers  = "MIME-Version: 1.0\n";
-$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-$headers .= "From: $nome <$email>";
+  // Emails para quem será enviado o formulário
+  $destino = "edu.3532quirino@gmail.com";
+  $assuntoEmail = "Contato pelo Site Portfólio";
 
-//Enviar
-if (mail($destino, $assunto, $arquivo, $headers)) {
-  echo "E-mail enviado com sucesso!";
+  // Cabeçalhos do e-mail
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=UTF-8\n";
+  $headers .= "From: $nome <$email>";
+
+  // Enviar e-mail
+  if (mail($destino, $assuntoEmail, $arquivo, $headers)) {
+    echo "E-mail enviado com sucesso!";
+  } else {
+    echo "Houve um erro ao enviar o e-mail. Por favor, tente novamente mais tarde.";
+  }
 } else {
-  echo "Houve um erro ao enviar o e-mail. Por favor, tente novamente mais tarde.";
+  echo "Acesso inválido ao script.";
 }
+?>
